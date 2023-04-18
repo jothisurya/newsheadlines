bbcb31997b8fa205be03ae15969bd64145b01e34
jothisurya <suryajothi1542@gmail.com>
2023-04-18 18:08:41 +0530

    final commit

---
+++ app/src/main/java/com/example/newsheadlines/Model.kt
@@ -0,0 +1,8 @@
+package com.example.newsheadlines
+
+data class Model(
+    val name: String,
+    val imageUrl: String,
+    val desc: String,
+    val category: String
+)
---
+++ app/src/main/java/com/example/newsheadlines/Source.kt
@@ -0,0 +1,6 @@
+package com.example.newsheadlines
+import com.google.gson.annotations.SerializedName
+data class Source(
+    @SerializedName("id"   ) var id   : String? = null,
+    @SerializedName("name" ) var name : String? = null
+)
